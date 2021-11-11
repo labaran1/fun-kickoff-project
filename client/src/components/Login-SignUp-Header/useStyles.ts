abbaa5a11e99be8887b7238bec9d8beca663c8fc
labaran1 <adlabaran@gmail.com>
2021-11-11 00:00:04 +0000

    styles for login and signup header

---
+++ client/src/components/Login-SignUp-Header/useStyles.ts
@@ -0,0 +1,47 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { orange } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+  nav: {
+    display: 'flex',
+    width: '100vw',
+    background: '#fff',
+    padding: '20px',
+    justifyContent: 'space-between',
+  },
+  link: { textDecoration: 'none' },
+
+  linkContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  sitterLink: {
+    margin: 'auto',
+    color: '#000',
+  },
+  selected: {
+    color: '#fff',
+    background: 'red',
+    border: 'none',
+    marginLeft: '10px',
+    width: '120px',
+    padding: '10px',
+    '&:hover': {
+      background: 'red',
+    },
+  },
+  unselected: {
+    color: 'red',
+    background: '#fff',
+    borderColor: 'red',
+    marginLeft: '10px',
+    width: '120px',
+    padding: '10px',
+
+    '&:hover': {
+      background: '#fff',
+    },
+  },
+}));
+
+export default useStyles;
